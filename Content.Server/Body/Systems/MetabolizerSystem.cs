eae07599d3da5c6f8dc05ff2627ec1bd949696dd
rhailrake <splatt.pr@gmail.com>
2026-02-27 14:02:07 +0500

    Revert "Replace metabolism groups with metabolism stages (#42172)"
    This reverts commit ff1af35afba1c494efff277acb6c7d79865ad403.

--- Content.Server/Body/Systems/MetabolizerSystem.cs
+++ Content.Server/Body/Systems/MetabolizerSystem.cs
@@ -1,8 +1,9 @@
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using Content.Shared.Body.Events;
-using Content.Shared.Body.Systems;
+using Content.Server.Body.Components;
 using Content.Shared.Body;
+using Content.Shared.Body.Events;
+using Content.Shared.Body.Prototypes;
+using Content.Shared.Body.Systems;
 using Content.Shared.Chemistry.Components;
 using Content.Shared.Chemistry.Components.SolutionManager;
 using Content.Shared.Chemistry.EntitySystems;
@@ -20,10 +21,10 @@ using Robust.Shared.Prototypes;
 using Robust.Shared.Random;
 using Robust.Shared.Timing;
 
-namespace Content.Shared.Metabolism;
+namespace Content.Server.Body.Systems;
 
 /// <inheritdoc/>
-public sealed class MetabolizerSystem : EntitySystem
+public sealed class MetabolizerSystem : SharedMetabolizerSystem
 {
     [Dependency] private readonly IGameTiming _gameTiming = default!;
     [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
@@ -35,6 +36,7 @@ public sealed class MetabolizerSystem : EntitySystem
 
     private EntityQuery<OrganComponent> _organQuery;
     private EntityQuery<SolutionContainerManagerComponent> _solutionQuery;
+    private static readonly ProtoId<MetabolismGroupPrototype> Gas = "Gas";
 
     public override void Initialize()
     {
@@ -43,6 +45,7 @@ public sealed class MetabolizerSystem : EntitySystem
         _organQuery = GetEntityQuery<OrganComponent>();
         _solutionQuery = GetEntityQuery<SolutionContainerManagerComponent>();
 
+        SubscribeLocalEvent<MetabolizerComponent, ComponentInit>(OnMetabolizerInit);
         SubscribeLocalEvent<MetabolizerComponent, MapInitEvent>(OnMapInit);
         SubscribeLocalEvent<MetabolizerComponent, BodyRelayedEvent<ApplyMetabolicMultiplierEvent>>(OnApplyMetabolicMultiplier);
     }
@@ -50,6 +53,18 @@ public sealed class MetabolizerSystem : EntitySystem
     private void OnMapInit(Entity<MetabolizerComponent> ent, ref MapInitEvent args)
     {
         ent.Comp.NextUpdate = _gameTiming.CurTime + ent.Comp.AdjustedUpdateInterval;
+    }
+
+    private void OnMetabolizerInit(Entity<MetabolizerComponent> entity, ref ComponentInit args)
+    {
+        if (!entity.Comp.SolutionOnBody)
+        {
+            _solutionContainerSystem.EnsureSolution(entity.Owner, entity.Comp.SolutionName, out _);
+        }
+        else if (_organQuery.CompOrNull(entity)?.Body is { } body)
+        {
+            _solutionContainerSystem.EnsureSolution(body, entity.Comp.SolutionName, out _);
+        }
     }
 
     private void OnApplyMetabolicMultiplier(Entity<MetabolizerComponent> ent, ref BodyRelayedEvent<ApplyMetabolicMultiplierEvent> args)
@@ -80,86 +95,56 @@ public sealed class MetabolizerSystem : EntitySystem
         }
     }
 
-    /// <summary>
-    /// Updates the metabolic rate multiplier for a given entity,
-    /// raising both <see cref="GetMetabolicMultiplierEvent"/> to determine what the multiplier is and <see cref="ApplyMetabolicMultiplierEvent"/> to update relevant components.
-    /// </summary>
-    /// <param name="uid"></param>
-    public void UpdateMetabolicMultiplier(EntityUid uid)
+    private void TryMetabolize(Entity<MetabolizerComponent, OrganComponent?, SolutionContainerManagerComponent?> ent)
     {
-        var getEv = new GetMetabolicMultiplierEvent();
-        RaiseLocalEvent(uid, ref getEv);
+        _organQuery.Resolve(ent, ref ent.Comp2, logMissing: false);
 
-        var applyEv = new ApplyMetabolicMultiplierEvent(getEv.Multiplier);
-        RaiseLocalEvent(uid, ref applyEv);
-    }
+        // First step is get the solution we actually care about
+        var solutionName = ent.Comp1.SolutionName;
+        Solution? solution = null;
+        Entity<SolutionComponent>? soln = default!;
+        EntityUid? solutionEntityUid = null;
 
-    private bool LookupSolution(
-        Entity<MetabolizerComponent, OrganComponent?, SolutionContainerManagerComponent?> ent,
-        MetabolismSolutionEntry solutionData,
-        bool lookupTransfer,
-        [NotNullWhen(true)] out Solution? solution,
-        [NotNullWhen(true)] out Entity<SolutionComponent>? solutionEntity,
-        [NotNullWhen(true)] out EntityUid? solutionOwner
-    )
-    {
-        solution = null;
-        solutionEntity = null;
-        solutionOwner = null;
-
-        var solutionName = lookupTransfer ? solutionData.TransferSolutionName : solutionData.SolutionName;
-
-        if (solutionName is null)
-            return false;
-
-        if (lookupTransfer ? solutionData.TransferSolutionOnBody : solutionData.SolutionOnBody)
+        if (ent.Comp1.SolutionOnBody)
         {
             if (ent.Comp2?.Body is { } body)
             {
-                if (!_solutionQuery.TryComp(body, out var bodySolution))
-                    return false;
+                if (!_solutionQuery.Resolve(body, ref ent.Comp3, logMissing: false))
+                    return;
 
-                solutionOwner = body;
-                return _solutionContainerSystem.TryGetSolution((body, bodySolution), solutionName, out solutionEntity, out solution);
+                _solutionContainerSystem.TryGetSolution((body, ent.Comp3), solutionName, out soln, out solution);
+                solutionEntityUid = body;
             }
         }
         else
         {
             if (!_solutionQuery.Resolve(ent, ref ent.Comp3, logMissing: false))
-                return false;
+                return;
 
-            solutionOwner = ent;
-            return _solutionContainerSystem.TryGetSolution((ent, ent), solutionName, out solutionEntity, out solution);
+            _solutionContainerSystem.TryGetSolution((ent, ent), solutionName, out soln, out solution);
+            solutionEntityUid = ent;
         }
 
-        return false;
-    }
-
-    private void TryMetabolizeStage(Entity<MetabolizerComponent, OrganComponent?, SolutionContainerManagerComponent?> ent, ProtoId<MetabolismStagePrototype> stage)
-    {
-        if (!ent.Comp1.Solutions.TryGetValue(stage, out var solutionData))
+        if (solutionEntityUid is null
+            || soln is null
+            || solution is null
+            || solution.Contents.Count == 0)
+        {
             return;
-
-        if (!LookupSolution(ent, solutionData, false, out var solution, out var solutionEntity, out var solutionOwner))
-            return;
-
-        if (solution.Contents.Count == 0)
-            return;
-
-        LookupSolution(ent, solutionData, true, out var transferSolution, out var transferSolutionEntity, out _);
+        }
 
         // Copy the solution do not edit the original solution list
         var list = solution.Contents.ToList();
 
         // Collecting blood reagent for filtering
         var ev = new MetabolismExclusionEvent();
-        RaiseLocalEvent(solutionOwner.Value, ref ev);
+        RaiseLocalEvent(solutionEntityUid.Value, ref ev);
 
         // randomize the reagent list so we don't have any weird quirks
         // like alphabetical order or insertion order mattering for processing
         _random.Shuffle(list);
 
-        var isDead = _mobStateSystem.IsDead(solutionOwner.Value);
+        bool isDead = _mobStateSystem.IsDead(solutionEntityUid.Value);
 
         int reagents = 0;
         foreach (var (reagent, quantity) in list)
@@ -171,16 +156,10 @@ public sealed class MetabolizerSystem : EntitySystem
             if (ev.Reagents.Contains(reagent))
                 continue;
 
-            if (proto.Metabolisms is null || !proto.Metabolisms.Metabolisms.TryGetValue(stage, out var entry))
+            var mostToRemove = FixedPoint2.Zero;
+            if (proto.Metabolisms is null)
             {
-                var mostToTransfer = FixedPoint2.Clamp(solutionData.TransferRate, 0, quantity);
-
-                if (transferSolution is not null)
-                {
-                    solution.RemoveReagent(reagent, mostToTransfer);
-                    transferSolution.AddReagent(reagent, mostToTransfer * solutionData.TransferEfficacy);
-                }
-                else
+                if (ent.Comp1.RemoveEmpty)
                 {
                     solution.RemoveReagent(reagent, FixedPoint2.New(1));
                 }
@@ -188,58 +167,73 @@ public sealed class MetabolizerSystem : EntitySystem
                 continue;
             }
 
-            var rate = solutionData.MetabolizeAll ? quantity : entry.MetabolismRate;
-
-            // Remove $rate, as long as there's enough reagent there to actually remove that much
-            var mostToRemove = FixedPoint2.Clamp(rate, 0, quantity);
-
             // we're done here entirely if this is true
             if (reagents >= ent.Comp1.MaxReagentsProcessable)
                 return;
 
-            var scale = (float) mostToRemove;
-            if (!solutionData.MetabolizeAll)
-                scale /= (float) rate;
 
-            // if it's possible for them to be dead, and they are,
-            // then we shouldn't process any effects, but should probably
-            // still remove reagents
-            if (isDead && !proto.WorksOnTheDead)
+            // loop over all our groups and see which ones apply
+            if (ent.Comp1.MetabolismGroups is null)
                 continue;
 
-            var actualEntity = ent.Comp2?.Body ?? solutionOwner.Value;
-
-            // do all effects, if conditions apply
-            foreach (var effect in entry.Effects)
+            // TODO: Kill MetabolismGroups!
+            foreach (var group in ent.Comp1.MetabolismGroups)
             {
-                if (scale < effect.MinScale)
+                if (!proto.Metabolisms.TryGetValue(group.Id, out var entry))
                     continue;
 
-                if (effect.Probability < 1.0f && !_random.Prob(effect.Probability))
+                var rate = entry.MetabolismRate * group.MetabolismRateModifier;
+
+                // Remove $rate, as long as there's enough reagent there to actually remove that much
+                mostToRemove = FixedPoint2.Clamp(rate, 0, quantity);
+
+                var scale = (float) mostToRemove;
+
+                // TODO: This is a very stupid workaround to lungs heavily relying on scale = reagent quantity. Needs lung and metabolism refactors to remove.
+                // TODO: Lungs just need to have their scale be equal to the mols consumed, scale needs to be not hardcoded either and configurable per metabolizer...
+                if (group.Id != Gas)
+                    scale /= (float) entry.MetabolismRate;
+
+                // if it's possible for them to be dead, and they are,
+                // then we shouldn't process any effects, but should probably
+                // still remove reagents
+                if (isDead && !proto.WorksOnTheDead)
                     continue;
 
-                // See if conditions apply
-                if (effect.Conditions != null && !CanMetabolizeEffect(actualEntity, ent, solutionEntity.Value, effect.Conditions))
-                    continue;
+                var actualEntity = ent.Comp2?.Body ?? solutionEntityUid.Value;
 
-                ApplyEffect(effect);
-
-            }
-
-            // TODO: We should have to do this with metabolism. ReagentEffect struct needs refactoring and so does metabolism!
-            void ApplyEffect(EntityEffect effect)
-            {
-                switch (effect)
+                // do all effects, if conditions apply
+                foreach (var effect in entry.Effects)
                 {
-                    case ModifyLungGas:
-                        _entityEffects.ApplyEffect(ent, effect, scale);
-                        break;
-                    case AdjustReagent:
-                        _entityEffects.ApplyEffect(solutionEntity.Value, effect, scale);
-                        break;
-                    default:
-                        _entityEffects.ApplyEffect(actualEntity, effect, scale);
-                        break;
+                    if (scale < effect.MinScale)
+                        continue;
+
+                    if (effect.Probability < 1.0f && !_random.Prob(effect.Probability))
+                        continue;
+
+                    // See if conditions apply
+                    if (effect.Conditions != null && !CanMetabolizeEffect(actualEntity, ent, soln.Value, effect.Conditions))
+                        continue;
+
+                    ApplyEffect(effect);
+
+                }
+
+                // TODO: We should have to do this with metabolism. ReagentEffect struct needs refactoring and so does metabolism!
+                void ApplyEffect(EntityEffect effect)
+                {
+                    switch (effect)
+                    {
+                        case ModifyLungGas:
+                            _entityEffects.ApplyEffect(ent, effect, scale);
+                            break;
+                        case AdjustReagent:
+                            _entityEffects.ApplyEffect(soln.Value, effect, scale);
+                            break;
+                        default:
+                            _entityEffects.ApplyEffect(actualEntity, effect, scale);
+                            break;
+                    }
                 }
             }
 
@@ -250,32 +244,10 @@ public sealed class MetabolizerSystem : EntitySystem
 
                 // We have processed a reagant, so count it towards the cap
                 reagents += 1;
-
-                if (transferSolution is not null && entry.Metabolites is not null)
-                {
-                    foreach (var (metabolite, ratio) in entry.Metabolites)
-                    {
-                        transferSolution.AddReagent(metabolite, mostToRemove * ratio);
-                    }
-                }
             }
         }
 
-        _solutionContainerSystem.UpdateChemicals(solutionEntity.Value);
-        if (transferSolutionEntity is not null)
-        {
-            _solutionContainerSystem.UpdateChemicals(transferSolutionEntity.Value);
-        }
-    }
-
-    private void TryMetabolize(Entity<MetabolizerComponent, OrganComponent?, SolutionContainerManagerComponent?> ent)
-    {
-        _organQuery.Resolve(ent, ref ent.Comp2, logMissing: false);
-
-        foreach (var stage in ent.Comp1.Stages)
-        {
-            TryMetabolizeStage(ent, stage);
-        }
+        _solutionContainerSystem.UpdateChemicals(soln.Value);
     }
 
     /// <summary>
